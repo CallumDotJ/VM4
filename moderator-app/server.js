7a92cb6ed11f91fd02228e00ab870906ac2f5761
CallumDotJ <162898463+CallumDotJ@users.noreply.github.com>
2026-03-23 18:44:43 +0000

    express.json

--- moderator-app/server.js
+++ moderator-app/server.js
@@ -8,6 +8,9 @@ const PORT = process.env.MODERATOR_CONTAINER_INTERNAL ||3100;
 
 app.use('/', moderatorApiRouter);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, 'public'))); //serve static files from public directory
 
 app.listen(PORT, "0.0.0.0", () => {
